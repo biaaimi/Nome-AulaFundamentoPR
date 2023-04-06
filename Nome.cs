79266cd5ecebb938e6cb71c46249b156bcd75463
Beatriz Aimi <biagioni.aimi@gmail.com>
2023-04-06 11:23:53 -0300

    Add files via upload

---
+++ Nome.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Nome;
+            Console.WriteLine("Informe seu Nome: ");
+
+            Nome = Console.ReadLine();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine(string.Format("Meu nome é {0}", Nome));
+
+            Console.ReadKey();
+
+        }
+    }
+}
